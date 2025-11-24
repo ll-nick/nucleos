f97126426c4348b004a8e2f627c94ff70d23c1a8
ll-nick <68419636+ll-nick@users.noreply.github.com>
2025-11-24 13:34:47 +0100

    Implement the Module trait as well as the first basic modules

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,48 @@
+use std::fs;
+use std::path::Path;
+
 use mlua::{Lua, Result, Table};
+
+pub trait Module {
+    fn apply(&self) -> Result<()>;
+    fn undo(&self) -> Result<()>;
+}
+
+pub struct Echo {
+    pub message: String,
+}
+
+impl Module for Echo {
+    fn apply(&self) -> Result<()> {
+        println!("{}", self.message);
+        Ok(())
+    }
+
+    fn undo(&self) -> Result<()> {
+        // nothing to undo for echo
+        Ok(())
+    }
+}
+
+pub struct File {
+    pub path: String,
+}
+
+impl Module for File {
+    fn apply(&self) -> Result<()> {
+        fs::write(&self.path, b"Created by nucleos")?;
+        println!("File created: {}", self.path);
+        Ok(())
+    }
+
+    fn undo(&self) -> Result<()> {
+        if Path::new(&self.path).exists() {
+            fs::remove_file(&self.path)?;
+            println!("File removed: {}", self.path);
+        }
+        Ok(())
+    }
+}
 
 fn main() -> Result<()> {
     // Initialize Lua
