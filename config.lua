5fb7f741fa81a2b3db36b4b76e91a9330c0efc78
ll-nick <68419636+ll-nick@users.noreply.github.com>
2025-11-28 20:02:08 +0100

    Format lua files with spaces, not tabs

--- config.lua
+++ config.lua
@@ -1,14 +1,14 @@
 return {
-	tasks = {
-		test_task = {
-			module = nucleos.builtin.file({
-				path = "/tmp/test.txt",
-			}),
-		},
-		another_task = {
-			module = nucleos.builtin.echo({
-				message = "Hello, World!",
-			}),
-		},
-	},
+    tasks = {
+        test_task = {
+            module = nucleos.builtin.file({
+                path = "/tmp/test.txt",
+            }),
+        },
+        another_task = {
+            module = nucleos.builtin.echo({
+                message = "Hello, World!",
+            }),
+        },
+    },
 }
